f9f846f6f0080855c1eaa22c64e4af3f9cbea8e4
Andrew Rogers <andrewprogers@gmail.com>
2019-05-25 09:01:27 -0700

    publish-domain-change

---
+++ docs/precache-manifest.dee0f84bea9c946c44f7968eb1fa3345.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c2088c463f2efb66261283d0ba698aa",
+    "url": "/static/media/details.2c2088c4.JPG"
+  },
+  {
+    "revision": "a21639e9837811b3518e",
+    "url": "/static/css/main.6bffbab6.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5035add5887feae3b5dd5b19c008da3a",
+    "url": "/static/media/splashPageLarge.5035add5.jpg"
+  },
+  {
+    "revision": "d9fe1574608041494b44",
+    "url": "/static/js/2.2b13316d.chunk.js"
+  },
+  {
+    "revision": "a21639e9837811b3518e",
+    "url": "/static/js/main.c7075d49.chunk.js"
+  },
+  {
+    "revision": "95ffb5e48f114f8573088324140d4a67",
+    "url": "/static/media/giftsLarge.95ffb5e4.jpg"
+  },
+  {
+    "revision": "074c114bf1566c48e2a58a64f8d8913e",
+    "url": "/static/media/rsvp.074c114b.jpg"
+  },
+  {
+    "revision": "d475b65251827dc8a42db314425b5491",
+    "url": "/static/media/splashPageSmall.d475b652.jpg"
+  },
+  {
+    "revision": "d9fe1574608041494b44",
+    "url": "/static/css/2.0f0fa7ff.chunk.css"
+  },
+  {
+    "revision": "9a0cba685416f2e812e48db37a8a8cce",
+    "url": "/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/andrew-stephanie-wedding/precache-manifest.97373317bcd5445aa0a10866df6942a3.js"
+  "/precache-manifest.dee0f84bea9c946c44f7968eb1fa3345.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/andrew-stephanie-wedding/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
