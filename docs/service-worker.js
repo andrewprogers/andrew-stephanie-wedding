3733dc9d3773783fce3b918701e1373f12aa54c1
Andrew Rogers <andrewprogers@gmail.com>
2019-05-25 09:12:49 -0700

    fix domain

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dee0f84bea9c946c44f7968eb1fa3345.js"
+  "/andrew-stephanie-wedding/precache-manifest.db0d04c11ab675a3a9720a80705eb031.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/andrew-stephanie-wedding/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
